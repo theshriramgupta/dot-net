7768477c0d9021390ca690c7b01d068582fe6516
theshriramgupta <guptashriram0305@gmail.com>
2025-08-15 18:52:41 +0530

    add MCP_Server Folder

---
+++ MCP_Server/SampleMcpServer/Tools/WeatherTools.cs
@@ -0,0 +1,22 @@
+[McpServerTool]
+[Description("Describes random weather in the provided city.")]
+
+public class WeatherTool
+{
+    [McpServerTool]
+    [Description("Describes random weather in the provided city.")]
+    public string GetCityWeather(
+        [Description("Name of the city to return weather for")] string city)
+    {
+        var weather = Environment.GetEnvironmentVariable("WEATHER_CHOICES");
+        if (string.IsNullOrWhiteSpace(weather))
+        {
+            weather = "balmy,rainy,stormy";
+        }
+
+        var weatherChoices = weather.Split(",");
+        var selectedWeatherIndex = Random.Shared.Next(0, weatherChoices.Length);
+
+        return $"The weather in {city} is {weatherChoices[selectedWeatherIndex]}.";
+    }
+}
