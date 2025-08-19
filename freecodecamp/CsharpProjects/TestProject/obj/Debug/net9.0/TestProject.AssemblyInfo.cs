28c9f49779acbdbf9d5e03764ac868bc8c8e4b49
theshriramgupta <guptashriram0305@gmail.com>
2025-08-19 12:48:40 +0530

    update

--- freecodecamp/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ freecodecamp/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2149bc6c3b1c0859ef1d61266d1f9df0938f621b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e553f1be3d129636490dd76cab183b9757e72cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
