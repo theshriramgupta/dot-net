f395d40bf767722216acfd30c8ad761337146ebe
theshriramgupta <guptashriram0305@gmail.com>
2025-09-20 23:44:21 +0530

    add React Applications

---
+++ ReactApp/RankingApp.Server/Models/ItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace RankingApp.Server.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int ImageId { get; set; }
+
+        public int Ranking { get; set; }
+
+        public int ItemType { get; set; }
+    }
+}
---
+++ ReactApp/RankingApp.Server/Pages/Error.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RankingApp.Server.Pages
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
